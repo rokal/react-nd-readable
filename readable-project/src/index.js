f276ee6fb2c40129e16b2966752e413d81e88687
Roland Kalmogo <Roland.Kalmogo@telus.com>
2017-11-26 09:20:05 -0500

    feat: made deleted posts inaccessible
    I display comments number in posts details and also made deleted post
    inaccessible in post details view

--- readable-project/src/index.js
+++ readable-project/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
-// import App from './App';
 import Readable from './Readable'
 import registerServiceWorker from './registerServiceWorker';
 
